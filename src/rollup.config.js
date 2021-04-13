33ecf3d7a3cff5a57705ff1dcc65418a3e195956
Wil Stead <wil.stead@gmail.com>
2021-04-13 15:36:41 -0400

    Replace missing rollup config

---
+++ src/rollup.config.js
@@ -0,0 +1,12 @@
+﻿﻿import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+
+export default [{
+    input: "./ts/editor.js",
+    output: {
+        file: "./wwwroot/editor.js",
+        format: 'es',
+        sourcemap: true,
+    },
+    plugins: [nodeResolve(), terser()],
+}];
